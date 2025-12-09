ec171afd9647ddaadb54bf57ccb6df2904940c87
P-Nastia <nastia.pyvovarchuck@gmail.com>
2025-12-09 21:09:00 +0200

    start working android simple task manager

---
+++ WebTaskApi/WebTaskApi/Controllers/ZadachiController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebTaskApi.Models.Zadacha;
+
+namespace WebTaskApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ZadachiController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetList()
+        {
+            var items = new List<ZadachaItemModel>
+            {
+                new ZadachaItemModel{Id = 1, Name = "Mop the floor"},
+                new ZadachaItemModel{Id = 2, Name = "Walk the dog"},
+                new ZadachaItemModel{Id = 3, Name = "Feed the dog"},
+            };
+            return Ok(items);
+        }
+    }
+}
---
+++ taskManager/app/src/main/java/com/example/mytaskmanager/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.mytaskmanager;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.mytaskmanager.dto.zadachi.ZadachaItemDTO;
+import com.example.mytaskmanager.network.RetrofitClient;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        RetrofitClient
+                .getInstance()
+                .getZadachiApi()
+                .list()
+                .enqueue(new Callback<List<ZadachaItemDTO>>() {
+                    @Override
+                    public void onResponse(Call<List<ZadachaItemDTO>> call, Response<List<ZadachaItemDTO>> response) {
+                        if(response.isSuccessful()){
+                            List<ZadachaItemDTO> items = response.body();
+                            int count = items.size();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<ZadachaItemDTO>> call, Throwable t) {
+
+                    }
+                });
+    }
+}
