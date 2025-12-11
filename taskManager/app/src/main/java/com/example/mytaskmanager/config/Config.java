dbf395542fe97cf8ff8508f69a04a8f54532fe4c
P-Nastia <nastia.pyvovarchuck@gmail.com>
2025-12-11 20:57:48 +0200

    work zadachi with bug

--- taskManager/app/src/main/java/com/example/mytaskmanager/config/Config.java
+++ taskManager/app/src/main/java/com/example/mytaskmanager/config/Config.java
@@ -1,5 +1,6 @@
 package com.example.mytaskmanager.config;
 
 public class Config {
-    public static final String BASE_URL = "http://192.168.0.100:5095";
+    public static final String BASE_URL = "http://192.168.0.100:5095/";
+    public static final String IMAGES_URL = BASE_URL + "images/";
 }
