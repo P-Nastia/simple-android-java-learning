a838d28f4a26cd6122f473afbbb56faaf3335460
P-Nastia <nastia.pyvovarchuck@gmail.com>
2025-12-16 18:24:23 +0200

    add crud operations

--- WebTaskApi/WebTaskApi/Mappers/ZadachaMapper.cs
+++ WebTaskApi/WebTaskApi/Mappers/ZadachaMapper.cs
@@ -11,5 +11,8 @@ public class ZadachaMapper : Profile
         CreateMap<ZadachaItemModel, ZadachaEntity>().ReverseMap();
         CreateMap<ZadachaCreateModel, ZadachaEntity>()
                 .ForMember(dest => dest.Image, opt => opt.Ignore());
+        CreateMap<ZadachaUpdateModel, ZadachaEntity>()
+            .ForMember(dest => dest.Image, opt => opt.Ignore())
+            .ForMember(dest => dest.Image, opt => opt.Ignore());
     }
 }
---
+++ WebTaskApi/WebTaskApi/Models/Zadacha/ZadachaUpdateModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebTaskApi.Models.Zadacha;
+
+public class ZadachaUpdateModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public IFormFile? Image { get; set; }
+}
